6905bcc7e8d03b7f7925b358709ebb9db48ca98b
Marcos Santana <86273127+marcos-santan4@users.noreply.github.com>
2025-06-18 02:07:23 +0000

    adiciona scripts `lint:ckeck` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
-    return (
-        <>
-        <h1>Foco, pois os seus objetivos dependem de você!</h1>
-        </>
-    )
+  return (
+    <>
+      <h1>Foco, pois os seus objetivos dependem de você!</h1>
+    </>
+  );
 }
